509b4e5a11b86dd2274ce440c29bc3f7e3ca3a60
Luke Fleming <61019950+bh37vk@users.noreply.github.com>
2021-05-27 18:55:33 +0100

    Movement/acceleration
    Movement allowing player to go left and right while there is a constant acceleration forward

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float acceleration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = -transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += Time.deltaTime * acceleration;
+        rb.velocity = transform.forward * speed;
+    }
+}
